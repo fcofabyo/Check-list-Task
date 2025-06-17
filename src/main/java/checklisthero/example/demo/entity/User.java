d4a38d1cf64f6c52b4b6ed1e7c19b716082f8d24
Fco fabio <franciscofabioinffo@gmail.com>
2025-06-17 10:23:48 -0300

    feat: Creation of the User entity

---
+++ src/main/java/checklisthero/example/demo/entity/User.java
@@ -0,0 +1,48 @@
+package checklisthero.example.demo.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    private Integer level;
+
+    private Integer xp;
+
+    private LocalDate createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDate.now();
+        this.level = 1;
+        this.xp = 0;
+    }
+}
