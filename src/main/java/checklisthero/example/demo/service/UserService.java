081ca5e7560e3016544d82428423599bd1ebaf04
Fco fabio <franciscofabioinffo@gmail.com>
2025-06-17 10:56:49 -0300

    feat: Creating a user service with UserService

---
+++ src/main/java/checklisthero/example/demo/service/UserService.java
@@ -0,0 +1,29 @@
+package checklisthero.example.demo.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import checklisthero.example.demo.entity.User;
+import checklisthero.example.demo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User registerUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+}
