de14efbfc1445dd2791ae5d92e062c1d50cc7274
Fco fabio <franciscofabioinffo@gmail.com>
2025-06-26 10:35:06 -0300

    feat: New frontend started

--- taksa/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-blue-500 text-white">
-      <h1 className="text-3xl font-bold">Tailwind funcionando!</h1>
-    </main>
-  );
-}
